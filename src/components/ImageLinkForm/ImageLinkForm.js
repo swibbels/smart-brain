402a264f41cc9356bf107946a1c8b3a7da1b6e32
swibbels <113222265+swibbels@users.noreply.github.com>
2023-10-02 13:15:29 -0500

    adding placeholder to image search box

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -9,7 +9,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
             </p>
             <div className='form center'>
                 <div className='pa4 br3 shadow-5 center'>
-                    <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} />
+                    <input className='f4 pa2 w-70 center' type='text' placeholder='Enter the URL of an image here' onChange={onInputChange} />
                     <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-blue'
                     onClick={onButtonSubmit}
                     >Detect</button>
